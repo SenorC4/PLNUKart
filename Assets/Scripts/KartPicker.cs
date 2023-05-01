214a6c20f69d73e2dfc79d59098805f0871f16e2
SenorC4 <44325992+SenorC4@users.noreply.github.com>
2023-04-30 17:04:22 -0700

    kart picker is much better now
    kart picker buttons now work (I used to have to click the gameobject)

--- Assets/Scripts/KartPicker.cs
+++ Assets/Scripts/KartPicker.cs
@@ -3,17 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class KartPicker : MonoBehaviour
 {
 
-    public GameObject kart1;
-    public GameObject kart2;
-    public GameObject kart3;
-
-    public Button kart1Button;
-    public Button kart2Button;
-    public Button kart3Button;
+    private List<GameObject> karts;
 
     public Vector3 rotation;
 
@@ -23,28 +18,57 @@ public class KartPicker : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        karts = new List<GameObject>();
+
+        foreach(GameObject kart in GameObject.FindGameObjectsWithTag("Player")){
+            karts.Add(kart);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        kart1.transform.Rotate(rotation * Time.deltaTime);
-        kart2.transform.Rotate(rotation * Time.deltaTime);
-        kart3.transform.Rotate(rotation * Time.deltaTime);
+        foreach(GameObject Kart in karts){
+            Kart.transform.Rotate(rotation * Time.deltaTime);
+        }
 
         if (Input.GetMouseButtonDown(0)) {
-                RaycastHit  hit;
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                if (Physics.Raycast(ray, out hit) && (hit.transform.name).Contains("2")) {
-                    Debug.Log( hit.transform.name);
-                    selected2 = hit.transform.name;
-                }else if(Physics.Raycast(ray, out hit)){
-                    Debug.Log( hit.transform.name + "single");
-                    selected = hit.transform.name;
-                }
+            RaycastHit  hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit) && (hit.transform.name).Contains("2")) {
+                //Debug.Log( hit.transform.name);
+                selected2 = hit.transform.name;
+            }else if(Physics.Raycast(ray, out hit)){
+                //Debug.Log( hit.transform.name + "single");
+                selected = hit.transform.name;
+            }
         }
+    }
+
+    
+    public void small(){
+        selected = "LightKart";
+    }
+
+    public void medium(){
+        selected = "MediumKart";
+    }
+
+    public void large(){
+        selected = "HeavyKart";
+    }
+
+   public void small2(){
+        selected2 = "LightKart2";
+    }
+
+    public void medium2(){
+        selected2 = "MediumKart2";
+    }
+
+    public void large2(){
+        selected2 = "HeavyKart2";
     }
 
     
