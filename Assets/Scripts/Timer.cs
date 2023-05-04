2e13684c0830b5dce0616336f016f91ff8b9f1d4
SenorC4 <44325992+SenorC4@users.noreply.github.com>
2023-05-03 21:18:52 -0700

    EndTimer works

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -8,8 +8,8 @@ public class Timer : MonoBehaviour
     public TMP_Text inGameTimer;
     public TMP_Text place;
 
-    public GameObject endTimer;
-    public GameObject endScreen;
+    private TMP_Text endTimer;
+    private GameObject endScreen;
 
     private float timer;
     //private MainMenu menu;
@@ -17,19 +17,16 @@ public class Timer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //menu = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<MainMenu>().getGameType();
-        //gameType = menu.getGameType();
         gameType = MainMenu.getGameType();
         endScreen = GameObject.FindGameObjectWithTag("EndScreen0");
-        endTimer = GameObject.FindGameObjectWithTag("EndTimer");
-
+        endTimer = GameObject.FindGameObjectWithTag("TimerText").GetComponent<TMP_Text>();
+        
+        
         if(gameType != "TimeTrial"){
             inGameTimer.gameObject.SetActive(false);
             place.gameObject.SetActive(true);
-            endTimer.SetActive(false);
+            
         }else{
-            
-            
             inGameTimer.gameObject.SetActive(true);
             place.gameObject.SetActive(false);
             
@@ -40,13 +37,12 @@ public class Timer : MonoBehaviour
     void Update()
     {
         if(Movement.getStarted()){
-            //Debug.Log(gameType);
+            
             timer += Time.deltaTime;
             if (gameType == "TimeTrial")
             {
                 inGameTimer.text = timer.ToString("F2");
-                
-                endTimer.GetComponent<TMP_Text>().text = timer.ToString("F2");
+                endTimer.text = timer.ToString("F2");
             }
         }
     }
